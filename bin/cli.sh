0b23c1d0b3595a0e691edd9984c987bc09152e53
Francisco Calle <francisco.calle.next@bbva.com>
2022-02-16 13:16:17 +0100

    Revert "fix(bin): read project name first"
    This reverts commit c2e2f186c65bf99dd717352b14f373c1f5b4638c.

--- bin/cli.sh
+++ bin/cli.sh
@@ -7,22 +7,21 @@ function egress {
 trap egress EXIT
 
 PATH_ZX=$(which zx)
+
 if [ ! -n "$PATH_ZX" ]
 then
   echo "@google/zx not found. Installing ..."
   npm i -g zx
 fi
 
-echo "Type the name of the project project:"
-printf "> "
-read PROJECTPATH
-
-
 CWD=$(pwd)
 git clone https://github.com/bitIO/create-typescript-project.git ./.tmp > /dev/null 2>&1
 cd ./.tmp && npm install --no-package-lock &>/dev/null && cd ..
 ZXPROJECTPATH=$CWD/.tmp
 
-echo $PROJECTPATH | npx typescript-starter
+npx typescript-starter
+echo "Type the relative path of the created project:"
+printf "> "
+read PROJECTPATH
 
 zx $ZXPROJECTPATH/index.mjs --starter $ZXPROJECTPATH --project $CWD/$PROJECTPATH
