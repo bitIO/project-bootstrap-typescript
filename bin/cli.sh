94dc515737a373c6a97a5b195b084ec798c6cc29
Francisco Calle Moreno <francisco.calle.moreno@gmail.com>
2022-02-16 11:59:17 +0100

    fix: install local dependencies

--- bin/cli.sh
+++ bin/cli.sh
@@ -1,4 +1,4 @@
-#!/bin/env
+#!/bin/sh
 
 function egress {
   echo "Cleaning out the mess ..."
@@ -16,8 +16,8 @@ fi
 
 CWD=$(pwd)
 git clone https://github.com/bitIO/create-typescript-project.git ./.tmp > /dev/null 2>&1
-# ZXPROJECTPATH=$CWD/.tmp
-ZXPROJECTPATH=$CWD/../
+cd ./.tmp && npm install --no-package-lock &>/dev/null && cd ..
+ZXPROJECTPATH=$CWD/.tmp
 
 npx typescript-starter
 echo "Type the relative path of the created project:"
