8fa0cdd6005f638148f0376ae413b3b68bd924e3
Francisco Calle <francisco.calle.next@bbva.com>
2022-02-16 11:33:13 +0100

    fix: simplify package.json shaping

--- index.mjs
+++ index.mjs
@@ -1,12 +1,8 @@
 #!/usr/bin/env zx
 
-// let sourcesPath = await question(chalk.green("Type the path of zx project: "));
-let sourcesPath = await $`pwd`;
-// let projectPath = await question(
-//   chalk.green("Type the path of the created folder: ")
-// );
-let projectPath =
-  "/Users/e045756/Dev/personal/setup-typescript-project/logger-pino";
+let sourcesPath = argv.starter;
+let projectPath = argv.project;
+
 cd(projectPath);
 
 // REMOVE: initial stuff
@@ -20,9 +16,10 @@ await Promise.all([
   $`echo > ./src/index.ts`,
 ]);
 
-// FIX: package
+console.log("--- SETUP PROJECT ---");
+// SETUP: package.json
 // -----------------------------------------------------------------------------
-await $`node ../src/package.js ${projectPath}`;
+await $`node ${sourcesPath}/src/package.js ${projectPath}`;
 await Promise.all([
   await $`cp ${sourcesPath}/assets/.npmrc .`,
   await $`cp ${sourcesPath}/assets/.yarnrc .`,
@@ -30,22 +27,8 @@ await Promise.all([
 
 // FIX: versions
 // -----------------------------------------------------------------------------
-console.log("--- FIX PACKAGE VERSIONS ---");
 await $`sed -i -e 's/"^/"/g' ${projectPath}/package.json `;
 await $`rm -f ${projectPath}/package.json-* `;
-
-// SETUP: required dependencies
-// -----------------------------------------------------------------------------
-console.log("--- ADD REQUIRED EPENDENCIES ---");
-await $`yarn add --silent -D @types/node husky @commitlint/cli @commitlint/config-conventional jest @types/jest ts-jest jest-html-reporters`;
-console.log("--- UPDATE EXISTING DEPENDENCIES ---");
-await $`ncu -u`;
-await $`rm -rf node_modules yarn.lock package-lock.json`;
-await $`yarn install --silent --no-lockfile`;
-
-// REMOVE: doc gh-pages
-// -----------------------------------------------------------------------------
-await $`node ../src/docs.js ${projectPath}`;
 
 // SETUP: gitignore
 // -----------------------------------------------------------------------------
@@ -60,15 +43,11 @@ await $`cp ${sourcesPath}/assets/.eslintrc.json .`;
 // SETUP: prettier
 // -----------------------------------------------------------------------------
 await $`cp ${sourcesPath}/assets/.prettierrc.js .`;
-await $`node ../src/prettier.js ${projectPath}`;
 
 // SETUP: commit-lint
 // -----------------------------------------------------------------------------
 await $`cp ${sourcesPath}/assets/commitlint.config.js .`;
-await $`npx husky install`;
-await $`cp ${sourcesPath}/assets/husky/* .husky/`;
 await $`cp ${sourcesPath}/assets/lint-staged.config.js .`;
-await $`node ../src/commitlint.js ${projectPath}`;
 
 // SETUP: jest
 // -----------------------------------------------------------------------------
@@ -76,4 +55,15 @@ await $`cp ${sourcesPath}/assets/jest/* .`;
 await $`sed -i -e 's/"types": \\["node"],/"types": \\["node","jest"],/g' ${projectPath}/tsconfig.json `;
 await $`sed -i -e 's/"types": \\[],/"types": \\["node","jest"],/g' ${projectPath}/tsconfig.json `;
 await $`rm -f ${projectPath}/tsconfig.json-* `;
-await $`node ../src/jest.js ${projectPath}`;
+
+// SETUP:update dependencies
+// -----------------------------------------------------------------------------
+console.log("--- UPDATE EXISTING DEPENDENCIES ---");
+await $`ncu -u`;
+await $`rm -rf node_modules yarn.lock package-lock.json`;
+await $`yarn install --silent --no-lockfile`;
+
+// SETUP: husky
+// -----------------------------------------------------------------------------
+await $`npx husky install`;
+await $`cp ${sourcesPath}/assets/husky/* .husky/`;
--- src/commitlint.js
+++
@@ -1,8 +0,0 @@
-const { loadJSON, saveJSON } = require("./json");
-
-console.log("--- COMMIT LINT ---");
-const packageFilePath = `${process.argv[2]}/package.json`;
-const json = loadJSON(packageFilePath);
-delete json.config;
-delete json.devDependencies["cz-conventional-changelog"];
-saveJSON(packageFilePath, json);
