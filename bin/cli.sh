a425afb6a40c2a1eeee21db967781d667f619f4b
Francisco Calle Moreno <francisco.calle.moreno@gmail.com>
2022-02-16 13:05:10 +0100

    fix(bin): read project name first

--- bin/cli.sh
+++ bin/cli.sh
@@ -7,21 +7,22 @@ function egress {
 trap egress EXIT
 
 PATH_ZX=$(which zx)
-
 if [ ! -n "$PATH_ZX" ]
 then
   echo "@google/zx not found. Installing ..."
   npm i -g zx
 fi
 
+echo "Type the name of the project project:"
+printf "> "
+read PROJECTPATH
+
+
 CWD=$(pwd)
 git clone https://github.com/bitIO/create-typescript-project.git ./.tmp > /dev/null 2>&1
 cd ./.tmp && npm install --no-package-lock &>/dev/null && cd ..
 ZXPROJECTPATH=$CWD/.tmp
 
-npx typescript-starter
-echo "Type the relative path of the created project:"
-printf "> "
-read PROJECTPATH
+echo $PROJECTPATH | npx typescript-starter
 
 zx $ZXPROJECTPATH/index.mjs --starter $ZXPROJECTPATH --project $CWD/$PROJECTPATH
